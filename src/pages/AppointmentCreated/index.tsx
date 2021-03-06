734f00481f42b95d57b2fad14f3d627291b5eb7e
JúDCraide <juliadcraide31@gmail.com>
2021-03-06 00:32:42 -0300

    Criando páginas e rotas

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+
+const AppointmentCreated : React.FC = () => {
+
+
+  return <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+    <Text style={{color:'#999', fontSize:20}}>AppointmentCreated </Text>
+
+  </View>
+};
+
+export default AppointmentCreated ;
