9e68d64ec38719a8a61a97c1659090ead20aa04e
JúDCraide <juliadcraide31@gmail.com>
2021-03-10 00:49:52 -0300

    Criando estrutura da tela

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/core';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather'
 
+import { Container, Title, Description, OKButton, OKButtonText, } from './styles';
 
-const AppointmentCreated : React.FC = () => {
+const AppointmentCreated: React.FC = () => {
 
+  const { reset } = useNavigation();
 
-  return <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-    <Text style={{color:'#999', fontSize:20}}>AppointmentCreated </Text>
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    })
+  }, [])
 
-  </View>
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+      <Title>Agendamento concluído</Title>
+      <Description>, dia de de às </Description>
+      <OKButton onPress={handleOkPressed}>
+        <OKButtonText>Ok</OKButtonText>
+      </OKButton>
+    </Container>
+  )
 };
 
-export default AppointmentCreated ;
+export default AppointmentCreated;
