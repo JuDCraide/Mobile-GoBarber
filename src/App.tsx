d96e4f1ba56a59989694c2e3fb4d4efbc6aeccc3
JúDCraide <juliadcraide31@gmail.com>
2021-04-18 16:24:28 -0300

    Integrando CodePush ao código

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
+import codePush from 'react-native-code-push';
 
 import AppProvider from './hooks/index'
 import Routes from './routes'
 
 const App: React.FC = () => {
-  useEffect(()=>{
+  useEffect(() => {
     SplashScreen.hide()
-  },[])
-  return(
+  }, [])
+  return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312E38" /*translucent*/ />
       <AppProvider>
@@ -23,4 +24,6 @@ const App: React.FC = () => {
     </NavigationContainer>
   )
 };
-export default App;
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(App);
